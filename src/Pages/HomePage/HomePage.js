e91c196d44f7bd57a6a8fbdd1ce787c010c01287
Ovidijus <ovidijus.e@gmail.com>
2022-07-05 20:03:47 +0300

    no skills found

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -5,20 +5,19 @@ import { useHistory } from 'react-router-dom';
 import { baseUrl, myFetchAuth } from '../../utils';
 import Card from '../../components/Card/Card';
 import css from './HomePage.module.css';
-// import toast, { Toaster } from 'react-hot-toast';
+// import toast from 'react-hot-toast';
 
 function HomePage() {
   const history = useHistory();
   const { token } = useAuthCtx();
   if (!token) history.push('/login');
-  const [skills, setSkills] = useState([]);
+  const [skills, setSkills] = useState('');
 
   const getSkills = async (values) => {
     const fetchResult = await myFetchAuth(`${baseUrl}v1/content/skills`, 'GET', token, values);
 
     if (Array.isArray(fetchResult)) {
       setSkills(fetchResult);
-      // toast.loading('Loading skills...');
     }
   };
 
@@ -30,15 +29,30 @@ function HomePage() {
     <div className={css['cards-container']}>
       <h1 className={css['title']}>Home Page</h1>
       <div className={css['cards-output']}>
-        {/* {skills.length === 0 && <h2>No skills found! Add a new one.</h2>} */}
-        {skills.length === 0 && <h2>Loading skills...</h2>}
-        {skills.map((sObj) => (
-          <Card key={sObj.id} {...sObj} />
-        ))}
+        {!Array.isArray(skills) ? (
+          <h2>Loading...</h2>
+        ) : skills.length === 0 ? (
+          <h2>No skills found. Please add a new skill.</h2>
+        ) : (
+          skills.length > 0 && skills.map((skObj) => <Card key={skObj.id} {...skObj} />)
+        )}
       </div>
     </div>
   );
 }
+//   return (
+//     <div className={css['cards-container']}>
+//       <h1 className={css['title']}>Home Page</h1>
+//       <div className={css['cards-output']}>
+//         {/* {skills.length === 0 && <h2>No skills found! Add a new one.</h2>} */}
+//         {skills.length === 0 && <h2>Loading skills...</h2>}
+//         {skills.map((sObj) => (
+//           <Card key={sObj.id} {...sObj} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 
 //   return (
 //     <div className={css['cards-container']}>
